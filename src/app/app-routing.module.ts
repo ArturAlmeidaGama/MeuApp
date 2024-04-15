deffbca4eb330147d76681262b1f3eaa2fa90b24
ARTUR GAMA <artur.gama@etec.sp.gov.br>
2024-04-15 17:14:35 -0300

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,23 @@ const routes: Routes = [
   },
   {
     path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    loadChildren: () => import('./page1/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'tab1',
+    loadChildren: () => import('./tabs/tab1/tab1.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./tabs/tab2/tab2.module').then( m => m.Tab2PageModule)
+  },
+  {
+    path: 'tab3',
+    loadChildren: () => import('./tabs/tab3/tab3.module').then( m => m.Tab3PageModule)
+  },
+  {
+    path: 'page2',
+    loadChildren: () => import('./page2/page2.module').then( m => m.Page2PageModule)
   }
 ];
 
